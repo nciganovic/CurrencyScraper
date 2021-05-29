ca4bfea7f3b2e6e99eeb6f0038a01ba0d37a6c4a
nciganovic <nciganovic99@gmail.com>
2021-05-29 18:02:50 +0200

    remove space

--- CurrencyScraper/ConsoleApp/Currency.cs
+++ CurrencyScraper/ConsoleApp/Currency.cs
@@ -26,7 +26,7 @@ namespace ConsoleApp
 
         public string CurrencyName { get; set; }
         public string BuyingRate { get; set; }
-        public string  CashBuyingRate { get; set; }
+        public string CashBuyingRate { get; set; }
         public string SellingRate { get; set; }
         public string CashSellingRate { get; set; }
         public string MiddleRate { get; set; }
