52b7913a48b951bea72e27a3dc70208e442ca054
nciganovic <nciganovic99@gmail.com>
2021-05-29 17:49:00 +0200

    Add extension method for string

---
+++ CurrencyScraper/ConsoleApp/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class Extensions
+    {
+        public static int GetNthIndex(this string s, char characterToFind, int n)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == characterToFind)
+                {
+                    count++;
+                    if (count == n)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
